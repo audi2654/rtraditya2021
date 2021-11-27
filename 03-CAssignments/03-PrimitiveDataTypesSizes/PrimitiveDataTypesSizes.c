3ab437657aa3c7baf98f2ad8c860b314e3ad64d8
Aditya Panchal <adit2654@gmail.com>
2021-11-27 19:35:14 +0530

    C Assign Completion, Removed pdfs zips & unnecessary files

---
+++ 03-CAssignments/03-PrimitiveDataTypesSizes/PrimitiveDataTypesSizes.c
@@ -0,0 +1,13 @@
+//C Primitive Data Types assign.
+
+#include <stdio.h>
+
+int main(void)
+{
+    //code
+    printf("\n\n");
+
+    printf("Size of int = %ld bytes\n", sizeof(int));
+
+    return 0;
+}
