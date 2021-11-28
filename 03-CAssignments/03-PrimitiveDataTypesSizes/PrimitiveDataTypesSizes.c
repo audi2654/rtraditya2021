a307f80f7c51213a5a308800ea16e16c62c4fba3
Aditya Panchal <adit2654@gmail.com>
2021-11-28 18:03:17 +0530

    C assigment completion. removed obj & a.out files

--- 03-CAssignments/03-PrimitiveDataTypesSizes/PrimitiveDataTypesSizes.c
+++ 03-CAssignments/03-PrimitiveDataTypesSizes/PrimitiveDataTypesSizes.c
@@ -1,4 +1,4 @@
-//C Primitive Data Types assign.
+//C Primitive Data Types assign
 
 #include <stdio.h>
 
@@ -7,7 +7,15 @@ int main(void)
     //code
     printf("\n\n");
 
-    printf("Size of int = %ld bytes\n", sizeof(int));
+    printf("Size of int             = %ld bytes\n", sizeof(int));
+    printf("Size of unsigned int    = %ld bytes\n", sizeof(unsigned int));
+    printf("Size of long            = %ld bytes\n", sizeof(long));
+    printf("Size of long long       = %ld bytes\n", sizeof(long long));
+    
+    printf("Size of float           = %ld bytes\n", sizeof(float));
+    printf("Size of double          = %ld bytes\n", sizeof(double));
+    printf("Size of long double     = %ld bytes\n", sizeof(long double));
 
+    printf("\n\n");
     return 0;
 }
