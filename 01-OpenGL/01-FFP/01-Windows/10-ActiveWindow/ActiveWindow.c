becae607af4bd7fdd351376a229053276e9677a3
Aditya Panchal <adit2654@gmail.com>
2022-02-12 02:33:06 +0530

    active window

---
+++ 01-OpenGL/01-FFP/01-Windows/10-ActiveWindow/ActiveWindow.c
@@ -0,0 +1,97 @@
+// Date: 11/02/2022
+// RTR2021 Active Window
+// Program to check whether your window is active or not
+
+// cl.exe /c /EHsc ActiveWindow.c
+// link.exe ActiveWindow.obj user32.lib gdi32.lib /SUBSYSTEM:WINDOWS
+
+// header files
+#include <Windows.h>
+
+// global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);   // Window Procedure (can be renamed but WndProc is coding convention)
+
+// global var. decl.
+BOOL gbActiveWindow = FALSE;
+
+// entry point functions
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+    // variable declaration
+    WNDCLASSEX wndclass;
+    HWND hwnd;
+    MSG msg;
+    TCHAR szAppName[] = TEXT("MyWindow");
+
+    // code
+
+    // initialization of WNDCLASSEX struct
+    wndclass.cbSize = sizeof(WNDCLASSEX);
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.hInstance = hInstance;
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.lpszClassName = szAppName;
+    wndclass.lpszMenuName = NULL;
+    wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    // registering above initialized WNDCLASSEX struct
+    RegisterClassEx(&wndclass);
+
+    // create the window
+    hwnd = CreateWindow(szAppName,
+                        TEXT("Aditya RTR Window"),
+                        WS_OVERLAPPEDWINDOW,
+                        CW_USEDEFAULT,
+                        CW_USEDEFAULT,
+                        CW_USEDEFAULT,
+                        CW_USEDEFAULT,
+                        NULL,
+                        NULL,
+                        hInstance,
+                        NULL);
+
+    // showing the window
+    ShowWindow(hwnd, iCmdShow);
+
+    // updating the window
+    UpdateWindow(hwnd);
+
+    // message loop
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return((int)msg.wParam);
+}
+
+// callback function definition
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    // code
+    switch(iMsg)
+    {
+    case WM_SETFOCUS:
+        gbActiveWindow = TRUE;
+        break;
+
+    case WM_KILLFOCUS:
+        gbActiveWindow = FALSE;
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        break;
+    }
+
+    return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
