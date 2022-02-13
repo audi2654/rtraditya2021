48c1e3a8299a54050c2c6283ec33f2bbc46a8523
Aditya Panchal <adit2654@gmail.com>
2022-02-14 02:15:32 +0530

    OGL Triangle

---
+++ 01-OpenGL/01-FFP/01-Windows/14-Triangle/OGL.cpp
@@ -0,0 +1,427 @@
+//Date: 13/02/2022
+//RTR2021 OGL Triangle
+
+//from this application Window.c is OGL.cpp
+//we are not going to use any special feature of CPP still we use .cpp
+//because after some apps we are going to include a header file which has code in CPP
+//to avoid that error we will use extension .cpp from here onwards
+
+//header files
+#include <windows.h>
+#include <stdio.h>			//for file I/O functions
+#include <stdlib.h>			//for exit()
+#include "OGL.h"			//for icon
+
+//OpenGL header files
+#include <GL/gl.h>
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+//OpenGL libraries
+#pragma comment(lib, "OpenGL32.lib")
+
+//global func decl.
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//global var decl.
+//for fullscreen
+HWND ghwnd = NULL;
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+BOOL gbFullScreen = FALSE;
+
+//for centered window
+int xWindowPosition = 0;
+int yWindowPosition = 0;
+
+int cxWindowWidth = 800;
+int cyWindowHeight = 600;
+
+//for active window
+BOOL gbActiveWindow = FALSE;
+
+//for file I/O
+FILE* gpFile = NULL;
+
+//entry point func
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//function decl.
+	int initialize(void);
+	void display(void);
+	void update(void);
+	void uninitialize(void);
+
+	//local var decl.
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("AMP RTR Window");
+	BOOL bDone = FALSE;		//for special loop
+	int iRetVal = 0;
+
+	int cxScreenWidth, cyScreenHeight;
+	cxScreenWidth = GetSystemMetrics(SM_CXSCREEN);		//1366
+	cyScreenHeight = GetSystemMetrics(SM_CYSCREEN);     //768
+
+	//code
+	//file IO
+	if (fopen_s(&gpFile, "Log.txt", "w") != 0)      //fopen_s safe/secured
+	   //fopen_s(out parameter ptr for the file opened or created, "name of file to open or create / path of file to read/write", reason to open file "r", "w", "a", "r+", etc)
+	{
+		MessageBox(HWND_DESKTOP, "Creation of Log file failed. Exitting!", "File I/O Error", MB_OK);
+		exit(0);
+	}
+	else
+	{
+		fprintf(gpFile, "Window Log file CREATED successfully!\n");
+	}
+
+	//initialize wndclass struct
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(AMP_MYICON));
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(AMP_MYICON));
+
+	//register wndclass struct in memory
+	RegisterClassEx(&wndclass);
+
+	//create window
+	xWindowPosition = (cxScreenWidth / 2) - (cxWindowWidth / 2);
+	yWindowPosition = (cyScreenHeight / 2) - (cyWindowHeight / 2);
+
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szAppName,
+		TEXT("AMP OGL Triangle"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		xWindowPosition,
+		yWindowPosition,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+
+	//initialize
+	iRetVal = initialize();
+	if (iRetVal == -1)
+	{
+		fprintf(gpFile, "Choose Pixel Format FAILED\n");
+		uninitialize();
+	}
+	else if (iRetVal == -2)
+	{
+		fprintf(gpFile, "Set Pixel Format FAILED\n");
+		uninitialize();
+	}
+	else if (iRetVal == -3)
+	{
+		fprintf(gpFile, "Create OpenGL Rendering Context FAILED\n");
+		uninitialize();
+	}
+	else if (iRetVal == -4)
+	{
+		fprintf(gpFile, "Making OpenGL Context as Current Context FAILED\n");
+		uninitialize();
+	}
+	else
+	{
+		fprintf(gpFile, "initialize() SUCCESSFULL\n");
+	}
+
+	//show window
+	ShowWindow(hwnd, iCmdShow);
+
+	//foregrounding & focusing the window
+	SetForegroundWindow(hwnd);		//here ghwnd would also work, but its primary use is for funcs outside winmain, we should use local var hwnd here
+	SetFocus(hwnd);					//sends WM_SETFOCUS
+
+	//game loop
+	while (bDone == FALSE)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				bDone = TRUE;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (gbActiveWindow == TRUE)
+			{
+				//render the scene
+				display();
+
+				//update the scene
+				update();
+			}
+		}
+	}
+
+	return((int)msg.wParam);
+}
+
+//callback func definition
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//local func decl.
+	void ToggleFullScreen(void);
+	void resize(int, int);
+	void uninitialize(void);
+
+	//local var decl.
+
+	
+	//code
+	switch (iMsg)
+	{
+	case WM_SETFOCUS:
+		gbActiveWindow = TRUE;
+		break;
+
+	case WM_KILLFOCUS:
+		gbActiveWindow = FALSE;
+		break;
+
+	case WM_ERASEBKGND:
+		//break;
+		return(0); //causes some issue in this specific skeleton application, this was applicable in Old Windows version
+		//although it works in all other applications
+		
+		//What is WM_ERASEBKGND
+		//when you have WM_PAINT there is no need to specially/deliberately erase backgroud or handle erase bkgnd msg
+		//it is done automatically by FERASE member of paintstruct is always true & it does the work
+		//but since we are not using WM_PAINT in this OpenGL skeleton app no one erases bkgnd,
+		//we have to do everything using OpenGL
+		//we also don't want this call to go DefWndProc(), we do not break; we return(0); from here
+		//cause if it goes to DefWndProc(), the OS does the next painting (WM_PAINT internally) i.e erasing bkgnd
+		//we want to erase it using OpenGL/DirectX
+
+	case WM_CHAR:
+		switch (wParam)
+		{
+		case 'F':
+		case 'f':
+			ToggleFullScreen();
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case 27:
+			DestroyWindow(hwnd);
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_SIZE:	//WM_SIZE comes as soon as a window is resized & hence handling it
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_CLOSE:	//this comes when window is closed, WM_CLOSE comes just before WM_DESTROY
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		uninitialize();
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullScreen(void)
+{
+	//local var decl.
+	static DWORD dwStyle;   //static to sustain values across different func calls
+	static WINDOWPLACEMENT wp;
+	MONITORINFO mi;
+	//static BOOL bFullScreen = FALSE; OR bFullScreen = gbFullScreen; also works
+
+	//code
+	wp.length = sizeof(WINDOWPLACEMENT);
+
+	if (gbFullScreen == FALSE)  //bFullScreen also works
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			mi.cbSize = sizeof(MONITORINFO);
+
+
+			if (GetWindowPlacement(ghwnd, &wp) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);    //framechanged internally send WM_NCCALCSIZE
+			}
+			ShowCursor(FALSE);
+			gbFullScreen = TRUE;
+		}
+	}
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wp);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);     //0, 0, 0, 0 because everything is given in wp
+
+
+		ShowCursor(TRUE);
+		gbFullScreen = FALSE;
+	}
+}
+
+int initialize(void)
+{
+	//function declarations
+	
+	//variable declarations
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex = 0;
+
+	//code
+	//zeroing/initializing all member of pfd to 0
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	//initializing pfd struct
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+
+	//get Device Context
+	ghdc = GetDC(ghwnd);
+
+	//choose pixel format
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0)
+	{
+		return(-1);
+	}
+
+	//set the choosen pixel format
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+		return(-2);
+
+	//create OpenGL rendering context
+	ghrc = wglCreateContext(ghdc);				//calling first Windows Bridging API
+	if (ghrc == NULL)
+		return(-3);
+
+	//make the rendering context as the current context
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE)
+		return(-4);
+
+	//here starts OpenGL code
+	//clear the screen using blue color
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);	//RGBA
+
+	return(0);
+}
+
+void resize(int width, int height)
+{
+	//code
+	if (height == 0)
+		height = 1;		//we keep compulsorily min val as 1 to avoid divided by 0 in future code calls
+
+	glViewport(0, 0, GLsizei(width), GLsizei(height));
+}
+
+void display(void)
+{
+	//code
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-1.0f, -1.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 0.0f);
+	glEnd();
+
+	SwapBuffers(ghdc);
+}
+
+void update(void)
+{
+	//code
+
+}
+
+void uninitialize(void)
+{
+	//function declarations
+	void ToggleFullScreen(void);
+
+	//code
+	if (gbFullScreen)
+	{
+		ToggleFullScreen();		//window should not die in Fullscreen mode
+	}
+
+	if (wglGetCurrentContext() == ghrc)
+	{
+		wglMakeCurrent(NULL, NULL);
+	}
+
+	if (ghrc)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+
+	if (ghdc)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	if (ghwnd)
+	{
+		DestroyWindow(ghwnd);	//we destroy here if in WinMain call to initialize() fails, we can immediately call unintialize() there itself
+		ghwnd = NULL;
+	}
+
+	if (gpFile)
+	{
+		fprintf(gpFile, "Window Log file CLOSED successfully!\n");
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+}
