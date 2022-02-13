7585f0101b40a187f642a34dd71cbd84832a08f2
Aditya Panchal <adit2654@gmail.com>
2022-02-14 02:13:36 +0530

    OGL Skeleton / Template / Boilerplate

---
+++ 01-OpenGL/01-FFP/01-Windows/12-Skeleton/WindowSkeleton.c
@@ -0,0 +1,317 @@
+//Date: 13/02/2022
+//RTR2021 Skeleton Window
+
+//header files
+#include <windows.h>
+#include <stdio.h>			//for file I/O functions
+#include <stdlib.h>			//for exit()
+#include "WindowIcon.h"		//for icon
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+//global func decl.
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//global var decl.
+//for fullscreen
+HWND ghwnd = NULL;
+BOOL gbFullScreen = FALSE;
+
+//for centered window
+int xWindowPosition = 0;
+int yWindowPosition = 0;
+
+int cxWindowWidth = 600;
+int cyWindowHeight = 600;
+
+//for active window
+BOOL gbActiveWindow = FALSE;
+
+//for file I/O
+FILE* gpFile = NULL;
+
+//entry point func
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//function decl.
+	int initialize(void);
+	void display(void);
+	void update(void);
+
+	//local var decl.
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("AMP RTR Window");
+	BOOL bDone = FALSE;		//for special loop
+	int iRetVal = 0;
+
+	int cxScreenWidth, cyScreenHeight;
+	cxScreenWidth = GetSystemMetrics(SM_CXSCREEN);		//1366
+	cyScreenHeight = GetSystemMetrics(SM_CYSCREEN);     //768
+
+	//code
+	//file IO
+	if (fopen_s(&gpFile, "Log.txt", "w") != 0)      //fopen_s safe/secured
+	   //fopen_s(out parameter ptr for the file opened or created, "name of file to open or create / path of file to read/write", reason to open file "r", "w", "a", "r+", etc)
+	{
+		MessageBox(HWND_DESKTOP, "Creation of Log file failed. Exitting!", "File I/O Error", MB_OK);
+		exit(0);
+	}
+	else
+	{
+		fprintf(gpFile, "Window Log file CREATED successfully!\n");
+	}
+
+	//initialize wndclass struct
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(AMP_MYICON));
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(AMP_MYICON));
+
+	//register wndclass struct in memory
+	RegisterClassEx(&wndclass);
+
+	//create window
+	xWindowPosition = (cxScreenWidth / 2) - (cxWindowWidth / 2);
+	yWindowPosition = (cyScreenHeight / 2) - (cyWindowHeight / 2);
+
+	hwnd = CreateWindow(szAppName,
+		TEXT("AMP RTR2021 Window"),
+		WS_OVERLAPPEDWINDOW,
+		xWindowPosition,
+		yWindowPosition,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+
+	//initialize
+	iRetVal = initialize();
+
+	//show window
+	ShowWindow(hwnd, iCmdShow);
+
+	//foregrounding & focusing the window
+	SetForegroundWindow(hwnd);		//here ghwnd would also work, but its primary use is for funcs outside winmain, we should use local var hwnd here
+	SetFocus(hwnd);					//sends WM_SETFOCUS
+
+	//game loop
+	while (bDone == FALSE)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				bDone = TRUE;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (gbActiveWindow == TRUE)
+			{
+				//render the scene
+				display();
+
+				//update the scene
+				update();
+			}
+		}
+	}
+
+	return((int)msg.wParam);
+}
+
+//callback func definition
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//local func decl.
+	void ToggleFullScreen(void);
+	void resize(int, int);
+	void uninitialize(void);
+
+	//local var decl.
+
+	
+	//code
+	switch (iMsg)
+	{
+	case WM_SETFOCUS:
+		gbActiveWindow = TRUE;
+		break;
+
+	case WM_KILLFOCUS:
+		gbActiveWindow = FALSE;
+		break;
+
+	case WM_ERASEBKGND:
+		break;
+		//return(0); //causes some issue in this specific skeleton application, this was applicable in Old Windows version
+		//although it works in all other applications
+		
+		//What is WM_ERASEBKGND
+		//when you have WM_PAINT there is no need to specially/deliberately erase backgroud or handle erase bkgnd msg
+		//it is done automatically by FERASE member of paintstruct is always true & it does the work
+		//but since we are not using WM_PAINT in this OpenGL skeleton app no one erases bkgnd,
+		//we have to do everything using OpenGL
+		//we also don't want this call to go DefWndProc(), we do not break; we return(0); from here
+		//cause if it goes to DefWndProc(), the OS does the next painting (WM_PAINT internally) i.e erasing bkgnd
+		//we want to erase it using OpenGL/DirectX
+
+	case WM_CHAR:
+		switch (wParam)
+		{
+		case 'F':
+		case 'f':
+			ToggleFullScreen();
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case 27:
+			DestroyWindow(hwnd);
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_SIZE:	//WM_SIZE comes as soon as a window is resized & hence handling it
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_CLOSE:	//this comes when window is closed, WM_CLOSE comes just before WM_DESTROY
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		uninitialize();
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullScreen(void)
+{
+	//local var decl.
+	static DWORD dwStyle;   //static to sustain values across different func calls
+	static WINDOWPLACEMENT wp;
+	MONITORINFO mi;
+	//static BOOL bFullScreen = FALSE; OR bFullScreen = gbFullScreen; also works
+
+	//code
+	wp.length = sizeof(WINDOWPLACEMENT);
+
+	if (gbFullScreen == FALSE)  //bFullScreen also works
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			mi.cbSize = sizeof(MONITORINFO);
+
+
+			if (GetWindowPlacement(ghwnd, &wp) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);    //framechanged internally send WM_NCCALCSIZE
+			}
+			ShowCursor(FALSE);
+			gbFullScreen = TRUE;
+		}
+	}
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wp);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);     //0, 0, 0, 0 because everything is given in wp
+
+
+		ShowCursor(TRUE);
+		gbFullScreen = FALSE;
+	}
+}
+
+int initialize(void)
+{
+	//function declarations
+	
+	//variable declarations
+
+	//code
+
+	return(0);
+}
+
+void resize(int width, int height)
+{
+	//code
+	if (height == 0)
+		height = 1;		//we keep compulsorily min val as 1 to avoid divided by 0 in future code calls
+
+}
+
+void display(void)
+{
+	//code
+
+}
+
+void update(void)
+{
+	//code
+
+}
+
+void uninitialize(void)
+{
+	//function declarations
+	void ToggleFullScreen(void);
+
+	//code
+	if (gbFullScreen)
+	{
+		ToggleFullScreen();		//window should not die in Fullscreen mode
+	}
+
+	if (ghwnd)
+	{
+		DestroyWindow(ghwnd);	//we destroy here if in WinMain call to initialize() fails, we can immediately call unintialize() there itself
+		ghwnd = NULL;
+	}
+
+	if (gpFile)
+	{
+		fprintf(gpFile, "Window Log file CLOSED successfully!\n");
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+}
