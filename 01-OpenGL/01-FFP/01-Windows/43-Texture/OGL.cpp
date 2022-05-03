d901acb0faed16f0856989118bf01c83ccdb9e3e
Aditya Panchal <adit2654@gmail.com>
2022-05-03 11:39:03 +0530

    Kundali & Stone Texture WIP

---
+++ 01-OpenGL/01-FFP/01-Windows/43-Texture/OGL.cpp
@@ -0,0 +1,584 @@
+//Date: 24/04/2022
+//RTR2021 OGL TEXTURED Perspective Projection Blue Screen - Two3DShapes Rotation
+
+//header files
+#include <windows.h>
+#include <stdio.h>			//for file I/O functions
+#include <stdlib.h>			//for exit()
+#include "OGL.h"			//for icon
+#include <GL/glu.h>
+
+//OpenGL header files
+#include <GL/gl.h>
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+//OpenGL libraries
+#pragma comment(lib, "OpenGL32.lib")
+#pragma comment(lib, "glu32.lib")
+
+//global func decl.
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//global var decl.
+//for fullscreen
+HWND ghwnd = NULL;
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+BOOL gbFullScreen = FALSE;
+
+//for centered window
+int xWindowPosition = 0;
+int yWindowPosition = 0;
+
+int cxWindowWidth = 800;
+int cyWindowHeight = 600;
+
+//for active window
+BOOL gbActiveWindow = FALSE;
+
+//for file I/O
+FILE* gpFile = NULL;
+
+//for 2D rotation
+float anglePyramid = 0.0f;
+float angleCube = 0.0f;
+
+//for texture variables
+GLuint texture_kundali;
+GLuint texture_stone;
+
+//entry point func
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//function decl.
+	int initialize(void);
+	void display(void);
+	void update(void);
+	void uninitialize(void);
+
+	//local var decl.
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("AMP RTR Window");
+	BOOL bDone = FALSE;		//for special loop
+	int iRetVal = 0;
+
+	int cxScreenWidth, cyScreenHeight;
+	cxScreenWidth = GetSystemMetrics(SM_CXSCREEN);		//1366
+	cyScreenHeight = GetSystemMetrics(SM_CYSCREEN);     //768
+
+	//code
+	//file IO
+	if (fopen_s(&gpFile, "Log.txt", "w") != 0)      //fopen_s safe/secured
+	   //fopen_s(out parameter ptr for the file opened or created, "name of file to open or create / path of file to read/write", reason to open file "r", "w", "a", "r+", etc)
+	{
+		MessageBox(HWND_DESKTOP, "Creation of Log file failed. Exitting!", "File I/O Error", MB_OK);
+		exit(0);
+	}
+	else
+	{
+		fprintf(gpFile, "Window Log file CREATED successfully!\n");
+	}
+
+	//initialize wndclass struct
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(AMP_MYICON));
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(AMP_MYICON));
+
+	//register wndclass struct in memory
+	RegisterClassEx(&wndclass);
+
+	//create window
+	xWindowPosition = (cxScreenWidth / 2) - (cxWindowWidth / 2);
+	yWindowPosition = (cyScreenHeight / 2) - (cyWindowHeight / 2);
+
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szAppName,
+		TEXT("AMP OpenGL"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		xWindowPosition,
+		yWindowPosition,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+
+	//initialize
+	iRetVal = initialize();
+	if (iRetVal == -1)
+	{
+		fprintf(gpFile, "Choose Pixel Format FAILED\n");
+		uninitialize();
+	}
+	else if (iRetVal == -2)
+	{
+		fprintf(gpFile, "Set Pixel Format FAILED\n");
+		uninitialize();
+	}
+	else if (iRetVal == -3)
+	{
+		fprintf(gpFile, "Create OpenGL Rendering Context FAILED\n");
+		uninitialize();
+	}
+	else if (iRetVal == -4)
+	{
+		fprintf(gpFile, "Making OpenGL Context as Current Context FAILED\n");
+		uninitialize();
+	}
+	else if (iRetVal == -5)
+	{
+		fprintf(gpFile, "LoadGlTexture for Stone FAILED\n");
+		uninitialize();
+	}
+	else if (iRetVal == -6)
+	{
+		fprintf(gpFile, "LoadGlTexture for Kundali FAILED\n");
+		uninitialize();
+	}
+	else
+	{
+		fprintf(gpFile, "initialize() SUCCESSFULL\n");
+	}
+
+	//show window
+	ShowWindow(hwnd, iCmdShow);
+
+	//foregrounding & focusing the window
+	SetForegroundWindow(hwnd);		//here ghwnd would also work, but its primary use is for funcs outside winmain, we should use local var hwnd here
+	SetFocus(hwnd);					//sends WM_SETFOCUS
+
+	//game loop
+	while (bDone == FALSE)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				bDone = TRUE;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (gbActiveWindow == TRUE)
+			{
+				//render the scene
+				display();
+
+				//update the scene
+				update();
+			}
+		}
+	}
+
+	uninitialize();
+
+	return((int)msg.wParam);
+}
+
+//callback func definition
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//local func decl.
+	void ToggleFullScreen(void);
+	void resize(int, int);
+
+	//local var decl.
+
+	
+	//code
+	switch (iMsg)
+	{
+	case WM_SETFOCUS:
+		gbActiveWindow = TRUE;
+		break;
+
+	case WM_KILLFOCUS:
+		gbActiveWindow = FALSE;
+		break;
+
+	case WM_ERASEBKGND:
+		return(0);
+
+	case WM_CHAR:
+		switch (wParam)
+		{
+		case 'F':
+		case 'f':
+			ToggleFullScreen();
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case 27:
+			DestroyWindow(hwnd);
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_SIZE:	//WM_SIZE comes as soon as a window is resized & hence handling it
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_CLOSE:	//this comes when window is closed, WM_CLOSE comes just before WM_DESTROY
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullScreen(void)
+{
+	//local var decl.
+	static DWORD dwStyle;   //static to sustain values across different func calls
+	static WINDOWPLACEMENT wp;
+	MONITORINFO mi;
+	//static BOOL bFullScreen = FALSE; OR bFullScreen = gbFullScreen; also works
+
+	//code
+	wp.length = sizeof(WINDOWPLACEMENT);
+
+	if (gbFullScreen == FALSE)  //bFullScreen also works
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			mi.cbSize = sizeof(MONITORINFO);
+
+
+			if (GetWindowPlacement(ghwnd, &wp) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);    //framechanged internally send WM_NCCALCSIZE
+			}
+			ShowCursor(FALSE);
+			gbFullScreen = TRUE;
+		}
+	}
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wp);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);     //0, 0, 0, 0 because everything is given in wp
+
+
+		ShowCursor(TRUE);
+		gbFullScreen = FALSE;
+	}
+}
+
+int initialize(void)
+{
+	//function declarations
+	void resize(int, int);
+	BOOL LoadGlTexture(GLuint*, TCHAR[]);
+	
+	//variable declarations
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex = 0;
+
+	//code
+	//zeroing/initializing all member of pfd to 0
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	//initializing pfd struct
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+	pfd.cDepthBits = 32;	//24 bits can also be used
+
+	//get Device Context
+	ghdc = GetDC(ghwnd);
+
+	//choose pixel format
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0)
+	{
+		return(-1);
+	}
+
+	//set the choosen pixel format
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+		return(-2);
+
+	//create OpenGL rendering context
+	ghrc = wglCreateContext(ghdc);				//calling first Windows Bridging API
+	if (ghrc == NULL)
+		return(-3);
+
+	//make the rendering context as the current context
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE)
+		return(-4);
+
+	//here starts OpenGL code
+	//clear the screen using blue color
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);	//RGBA
+
+	//depth 3D related changes
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+		//above 3 lines are must, below 2 lines are optional
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	//texture related code
+	if(LoadGlTexture(&texture_stone, MAKEINTRESOURCE(IDBITMAP_STONE)) == FALSE)
+	{
+		return -5;
+	}
+
+	if(LoadGlTexture(&texture_kundali, MAKEINTRESOURCE(IDBITMAP_KUNDALI)) == FALSE)
+	{
+		return -6;
+	}
+
+	//enabling the texture
+	glEnable(GL_TEXTURE_2D);
+	
+	//warm up resize call
+	//without this maroon or brownish window comes first
+	resize(WIN_WIDTH - 16, WIN_HEIGHT - 39);
+	//16 is border size 8-8 right & left size
+	//39 is 8 top border, 23 menu bar, 8 bottom border of window
+	//all funcs give window size including its border
+
+	return(0);
+}
+
+void resize(int width, int height)
+{
+	//code
+	if (height == 0)
+		height = 1;		//we keep compulsorily min val as 1 to avoid divided by 0 in future code calls
+
+	glViewport(0, 0, GLsizei(width), GLsizei(height));
+
+	//Perspective Projection
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+}
+
+void display(void)
+{
+	//code
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glTranslatef(-1.5f, 0.0f, -6.0f);
+	glRotatef(anglePyramid, 0.0f, 1.0f, 0.0f);		//y axis spin rotation
+	glBegin(GL_TRIANGLES);
+	//3D shapes che coordinates detana, sides/faces la TUMCHYA samor nai aanaich, TUMHI swatah tya tya face/side samor jaych
+	//ek side la colors pn aju baju chya sides la lakshat theun dayche
+	
+	//front face
+	glVertex3f(0.0f, 1.0f, 0.0f);		//apex
+	glVertex3f(-1.0f, -1.0f, 1.0f);		//frontFace leftBottom
+	glVertex3f(1.0f, -1.0f, 1.0f);		//frontFace rightBottom
+
+	//right face
+	glVertex3f(0.0f, 1.0f, 0.0f);		//apex
+	glVertex3f(1.0f, -1.0f, 1.0f);		//rightFace leftBottom = frontFace rightBottom
+	glVertex3f(1.0f, -1.0f, -1.0f);		//rightFace rightBottom
+
+	//back face
+	glVertex3f(0.0f, 1.0f, 0.0f);		//apex
+	glVertex3f(1.0f, -1.0f, -1.0f);		//backFace leftBottom = rightFace rightBottom
+	glVertex3f(-1.0f, -1.0f, -1.0f);	//backFace rightBottom
+
+	//left face
+	glVertex3f(0.0f, 1.0f, 0.0f);		//apex
+	glVertex3f(-1.0f, -1.0f, -1.0f);	//leftFace leftBottom = backFace rightBottom
+	glVertex3f(-1.0f, -1.0f, 1.0f);		//leftFace rightBottom = frontFace leftBottom
+	glEnd();
+
+	glLoadIdentity();
+
+	glTranslatef(1.5f, 0.0f, -6.0f);
+	glScalef(0.75f, 0.75f, 0.75f);
+	glRotatef(angleCube, 1.0f, 1.0f, 1.0f);		//all triaxis rotation
+	glBegin(GL_QUADS);
+	//front face
+	glVertex3f(1.0f, 1.0f, 1.0f);		//Right Top
+	glVertex3f(-1.0f, 1.0f, 1.0f);		//Left Top
+	glVertex3f(-1.0f, -1.0f, 1.0f);		//Left Bottom
+	glVertex3f(1.0f, -1.0f, 1.0f);		//Right Bottom
+
+	//right face
+	glVertex3f(1.0f, 1.0f, -1.0f);		//rightFace rightTop
+	glVertex3f(1.0f, 1.0f, 1.0f);		//rightFace leftTop = frontFace rightTop
+	glVertex3f(1.0f, -1.0f, 1.0f);		//rightFace leftBottom = frontFace rightBottom
+	glVertex3f(1.0f, -1.0f, -1.0f);		//rightFace rightBottom
+
+	//back face
+	glVertex3f(-1.0f, 1.0f, -1.0f);		//backFace rightTop
+	glVertex3f(1.0f, 1.0f, -1.0f);		//backFace leftTop = rightFace rightTop
+	glVertex3f(1.0f, -1.0f, -1.0f);		//backFace leftBottom = rightFace rightBottom
+	glVertex3f(-1.0f, -1.0f, -1.0f);	//backFace rightBottom
+
+	//left face
+	glVertex3f(-1.0f, 1.0f, 1.0f);		//leftFace rightTop	= frontFace leftTop
+	glVertex3f(-1.0f, 1.0f, -1.0f);		//leftFace leftTop = backFace rightTop
+	glVertex3f(-1.0f, -1.0f, -1.0f);	//leftFace leftBottom = backFace rightBottom
+	glVertex3f(-1.0f, -1.0f, 1.0f);		//leftFace rightBottom = frontFace leftBottom
+
+	//top face
+	glVertex3f(1.0f, 1.0f, -1.0f);		//topFace rightTop = rightFace rightTop = backFace leftTop
+	glVertex3f(-1.0f, 1.0f, -1.0f);		//topFace leftTop = backFace rightTop
+	glVertex3f(-1.0f, 1.0f, 1.0f);		//topFace leftBottom = leftFace rightTop = frontFace leftTop
+	glVertex3f(1.0f, 1.0f, 1.0f);		//topFace rightBottom = rightFace leftTop = frontFace rightTop
+
+	//bottom face
+	glVertex3f(1.0f, -1.0f, -1.0f);		//bottomFace rightTop = rightFace rightBottom = backFace leftBottom
+	glVertex3f(-1.0f, -1.0f, -1.0f);	//bottomFace leftTop = leftFace leftBottom = backFace rightBottom
+	glVertex3f(-1.0f, -1.0f, 1.0f);		//bottomFace leftBottom = leftFace rightBottom = frontFace leftBottom
+	glVertex3f(1.0f, -1.0f, 1.0f);		//bottomFace rightBottom = rightFace leftBottom = frontFace rightBottom
+	
+	glEnd();
+
+	SwapBuffers(ghdc);
+}
+
+void update(void)
+{
+	//code
+	anglePyramid = anglePyramid + 0.1f;
+	/*if(anglePyramid >= 360.0f)
+	{
+		anglePyramid = anglePyramid - 360.0f;
+	}*/
+	
+	angleCube = angleCube + 0.1f;
+	if(angleCube >= 360.0f)
+	{
+		angleCube = angleCube - 360.0f;
+	}
+}
+
+void uninitialize(void)
+{
+	//function declarations
+	void ToggleFullScreen(void);
+
+	//code
+	if (gbFullScreen)
+	{
+		ToggleFullScreen();		//window should not die in Fullscreen mode
+	}
+
+	if (wglGetCurrentContext() == ghrc)
+	{
+		wglMakeCurrent(NULL, NULL);
+	}
+
+	if (ghrc)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+
+	if (ghdc)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	if (ghwnd)
+	{
+		DestroyWindow(ghwnd);	//we destroy here if in WinMain iRetVal = initialize(); fails, we can immediately call unintialize() there itself
+		ghwnd = NULL;
+	}
+
+	if (gpFile)
+	{
+		fprintf(gpFile, "Window Log file CLOSED successfully!\n");
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+}
+
+//definition for user defined function
+BOOL LoadGlTexture(GLuint* texture, TCHAR ImageResourceID[])
+{
+	//local var decl.
+	HBITMAP hbitmap = NULL;
+	BITMAP bmp;
+	BOOL bResult = FALSE;
+
+	//code
+	hbitmap = (HBITMAP)LoadImage(GetModuleHandle(NULL), ImageResourceID, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+	//LR_CREATEDIBSECTION == returns a device independent bitmap section
+
+	if(hbitmap)
+	{
+		bResult = TRUE;
+
+		GetObject(hbitmap, sizeof(BITMAP), &bmp);
+
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+		//here we tell to unpack the pixels & arrange them in aligned manner of
+		//4 types that is RGBA
+
+		glGenTextures(1, texture);		//5th step
+
+		glBindTexture(GL_TEXTURE_2D, *texture);		//6th step
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); //7th step
+		//when texture is near to viewer we tell OGL to draw it with magnified/maximum details
+
+		glTextParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); //7th step
+		//when texture is far from viewer we tell OGL to draw it with minimum details
+		//for less load on OGL machine & increase performance
+
+		//creating the actual texture
+		gluBuild2DMipmaps(GL_TEXTURE_2D, 3, bmp.bmWidth, bmp.bmHeight, GL_BGR_EXT, GL_UNSIGNED_BYTE, bmp.bmBits);
+
+		
+	
+	}
+}
